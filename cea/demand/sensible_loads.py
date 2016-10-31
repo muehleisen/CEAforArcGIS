f78f60ecdb31cf0ab683ebe8ccc8dcd0ae8d02c1
Daren Thomas <thomas@arch.ethz.ch>
2016-10-31 16:48:20 +0100

    started to document `calc_Qgain_sen`

--- cea/demand/sensible_loads.py
+++ cea/demand/sensible_loads.py
@@ -257,24 +257,29 @@ def calc_Qhs_Qcs_sys_max(Af, prop_HVAC):
     return IC_max, IH_max
 
 
-"""
-=========================================
-solar and heat gains
-=========================================
-"""
+# =========================================
+# solar and heat gains
+# =========================================
 
 def calc_Qgain_sen(t, tsd, bpr, gv):
-
+    """
+    :param t:
+    :param tsd:
+    :param bpr:
+    :param gv:
+    :return:
+    """
     # internal loads
-    tsd['I_sol'][t], tsd['I_rad'][t]= calc_I_sol(t, bpr, tsd, gv)
+    tsd['I_sol'][t], tsd['I_rad'][t] = calc_I_sol(t, bpr, tsd, gv)
 
-    tsd['I_int_sen'][t] = tsd['people'][t] * bpr.internal_loads['Qs_Wp'] + 0.9 * (tsd['Ealf'][t] + tsd['Eprof'][t])\
+    tsd['I_int_sen'][t] = tsd['people'][t] * bpr.internal_loads['Qs_Wp'] + 0.9 * (tsd['Ealf'][t] + tsd['Eprof'][t]) \
                           + tsd['Qcdataf'][t] - tsd['Qcref'][t]
 
     # divide into components for RC model
     tsd['I_ia'][t] = 0.5 * tsd['I_int_sen'][t]
     tsd['I_m'][t] = (bpr.rc_model['Am'] / bpr.rc_model['Atot']) * (tsd['I_ia'][t] + tsd['I_sol'][t])
-    tsd['I_st'][t] = (1 - (bpr.rc_model['Am'] / bpr.rc_model['Atot']) - (bpr.rc_model['Htr_w'] / (9.1 * bpr.rc_model['Atot']))) * (tsd['I_ia'][t] + tsd['I_sol'][t])
+    tsd['I_st'][t] = (1 - (bpr.rc_model['Am'] / bpr.rc_model['Atot']) - (
+        bpr.rc_model['Htr_w'] / (9.1 * bpr.rc_model['Atot']))) * (tsd['I_ia'][t] + tsd['I_sol'][t])
 
     return tsd
 
