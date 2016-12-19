48f088c68bee53bb8bd8a07b400d6910423ec506
JIMENOFONSECA <fonseca@arch.ethz.ch>
2016-12-19 13:36:39 +0800

    almost there at least with uniform distributions

--- cea/demand/calibration/calibration_main.py
+++ cea/demand/calibration/calibration_main.py
@@ -16,6 +16,12 @@ import pandas as pd
 import pymc3 as pm
 from pymc3.backends import SQLite
 import theano.tensor as tt
+from theano import as_op
+from pymc3.distributions.distribution import Continuous, draw_values, generate_samples
+from pymc3.distributions.dist_math import bound
+from scipy import stats
+import numpy as np
+
 import theano
 from cea.demand import demand_main
 import matplotlib.pyplot as plt
@@ -35,7 +41,7 @@ __status__ = "Production"
 def calibration_main(locator, weather_path, building_name, variables, building_load, retrieve_results, niter):
 
     # create function of demand calculation and send to theano
-    @theano.compile.ops.as_op(itypes=[tt.dscalar, tt.dscalar, tt.dscalar, tt.dscalar], otypes=[tt.dvector])
+    @as_op(itypes=[tt.dscalar, tt.dscalar, tt.dscalar, tt.dscalar], otypes=[tt.dvector])
     def demand_calculation(phi, err, u_win, u_wall):
         gv.samples = {'U_win': u_win, 'U_wall': u_wall}
         gv.multiprocessing = False
@@ -58,16 +64,16 @@ def calibration_main(locator, weather_path, building_name, variables, building_l
         # add all variables to the model and assign a triangular distribution
         # for this we create a local variable out of the strings included in the list variables
         for variable in variables:
-            variable = Triangular('variable', c=pdf.loc[variable, 'mu'], lower=pdf.loc[variable, 'min'],
+            globals()[variable] = pm.Uniform(variable, lower=pdf.loc[variable, 'min'],
                                                     upper=pdf.loc[variable, 'max'])
-
+        print locals()
         # get priors for the model inaquacy and the measurement errors.
         phi = pm.Uniform('phi', lower=0, upper=0.01)
         err = pm.Uniform('err', lower=0, upper=0.02)
         sigma = pm.Normal('sigma', sd=1)
 
         # expected value of outcome
-        mu = pm.Deterministic('mu', demand_calculation(phi, err, u_win, u_wall))
+        mu = pm.Deterministic('mu', demand_calculation(phi, err, U_win, U_wall))
 
         # Likelihood (sampling distribution) of observations
         y_obs = pm.Normal('y_obs', mu=mu, sd=sigma, observed=obs_data)
@@ -87,10 +93,6 @@ def calibration_main(locator, weather_path, building_name, variables, building_l
 
 
 # Create Triangular distributions
-from pymc3.distributions.distribution import Continuous, draw_values, generate_samples
-from scipy import stats
-import numpy as np
-
 
 class Triangular(Continuous):
     R"""
@@ -127,10 +129,14 @@ class Triangular(Continuous):
         c = self.c
         lower = self.lower
         upper = self.upper
+        #f1 = -tt.log(2 * (value - lower) / ((upper - lower) * (c - lower)))
+        #f2 = -tt.log(2 / (upper - lower))
+        #f3 = -tt.log(2 * (upper - value) / ((upper - lower) * (upper - c)))
+        #return bound(lower <= value < c, f1, bound(value == c, f2, bound(c < value <= upper, f3)))
+
         return triang_logp(lower, upper, c, value)
 
-
-@theano.compile.ops.as_op(itypes=[tt.dscalar, tt.dscalar, tt.dscalar, tt.dscalar], otypes=[tt.dscalar])
+@as_op(itypes=[tt.dscalar, tt.dscalar, tt.dscalar, tt.dscalar], otypes=[tt.dscalar])
 def triang_logp(lower, upper, c, value):
     if lower <= value < c:
         return -tt.log(2 * (value - lower) / ((upper - lower) * (c - lower)))
@@ -149,8 +155,8 @@ def run_as_script():
     scenario_path = gv.scenario_reference
     locator = inputlocator.InputLocator(scenario_path=scenario_path)
     weather_path = locator.get_default_weather()
-    variables = {'U_win':None, 'U_roof':None, 'Ths_setb_C':None,
-                 'Ths_set_C':None}  # based on the variables listed in the uncertainty database and selected through a screeing process
+    variables = ['U_win', 'U_roof', 'Ths_setb_C',
+                 'Ths_set_C']  # based on the variables listed in the uncertainty database and selected through a screeing process
     building_name = 'B01'
     building_load = 'Qhsf_kWh'
     retrieve_results = False  # flag to retrieve and analyze results from calibration
